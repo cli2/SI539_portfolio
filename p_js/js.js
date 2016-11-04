88efea60e1484c4316fc4b4528aa1f62a0ed898a
Lc <apple@Lc-2.local>
2016-11-04 01:04:42 -0400

    Added javascript, and made index page and paragraphs responsive

---
+++ p_js/js.js
@@ -0,0 +1,15 @@
+function show_nav(){
+  document.getElementsByTagName('body').style.backgroundColor=
+}
+function cover_i(){
+  document.getElementById('index').style.backgroundImage = "url('" + 'cover.jpg' + "')";
+}
+function cover_d(){
+  document.getElementById('index').style.backgroundImage = "url('" + 'cover_d.jpg' + "')";
+}
+function cover_m(){
+  document.getElementById('index').style.backgroundImage = "url('" + 'cover_m.jpg' + "')";
+}
+function cover_g(){
+  document.getElementById('index').style.backgroundImage = "url('" + 'cover_g.jpg' + "')";
+}
