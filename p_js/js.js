5550390d9aef45759a1f40ce97d31c9715b2648f
Lc <apple@Lc-2.local>
2016-11-06 18:30:17 -0500

    Finished version for submission

--- p_js/js.js
+++ p_js/js.js
@@ -10,48 +10,14 @@ function cover_m(){
 function cover_g(){
   document.getElementById('index').style.backgroundImage = "url('" + './p_image/cover_g.jpg' + "')";
 }
+function cover_a(){
+  document.getElementById('index').style.backgroundImage = "url('" + './p_image/cover_a.jpg' + "')";
+}
 function show_nav(){
-  document.getElementById('full').style.opacity='0.95';
+  document.getElementById('full').style.opacity='1';
   document.getElementById('full').style.display='block';
 }
 function show_page(){
   document.getElementById('full').style.display='none';
-}
-
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('header').outerHeight();
-
-$(window).scroll(function(event){
-    didScroll = true;
-});
-
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 250);
-
-function hasScrolled() {
-    var st = $(this).scrollTop();
-
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $('header').removeClass('nav-down').addClass('nav-up');
-    } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('header').removeClass('nav-up').addClass('nav-down');
-        }
-    }
-
-    lastScrollTop = st;
+  document.getElementById('full').style.opacity='0';
 }
